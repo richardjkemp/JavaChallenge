d596f0000c042e221b6ca8da87dd262b0e37cb7d
richardjkemp <richardjkemp@hotmail.co.uk>
2016-08-26 16:33:56 +0100

    addition challenge complete

---
+++ src/addition.java
@@ -0,0 +1,28 @@
+
+public class addition {
+	
+	/**
+	 * Simple class that adds two numbers together 
+	 * author Richard Kemp
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		//Define variables which will be added together 
+		int x;
+		int y;
+		
+		//initialise variables with a value
+		x=1;
+		y=2;
+		
+		//define variable which will be the sum
+		int sum = x+y;
+		
+		//print sum to console
+		System.out.println(sum);
+
+	}
+
+}
